9adef311db8a902446981f838323b88da23eb51d
Talha9509 <mawtalha5112004@gmail.com>
2025-03-05 14:05:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+We all have played snake, water gun game in our childhood. If you haven’t, google the
+rules of this game and write a python program capable of playing this game with the
+user.
+1 for Snake
+0 for Water
+-1 for Gun
+'''
+
+import random
+# computer=-1
+computer=random.choice([1,-1,0])    #uses random module to chose random number
+youstr=input("Enter your choice: ")
+dict={"Snake":1,"Water":0,"Gun":-1}
+reversedict={1:"Snake",0:"Water",-1:"Gun"}
+
+you=dict[youstr]
+
+print(f"You chose {reversedict[you]} \nComputer chose {reversedict[computer]}")
+
+if (you==computer):
+    print("Draw")
+else:
+    if (computer==-1 and you==0):
+        print("You  Lose!")
+    elif (computer==-1 and you==1):
+        print("You  win!")
+    elif (computer==1 and you==0):
+        print("You  Lose!")
+    elif (computer==1 and you==-1):
+        print("You  Win!")
+    elif (computer==0 and you==1):
+        print("You  Win!")
+    elif (computer==0 and you==-1):
+        print("You  Lose!")
+    else:
+        print("Something went Wrong")
